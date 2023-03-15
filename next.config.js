a046db44fd03b48bcb4d9dc3daff52277b7d2999
crushh <2538990710@qq.com>
2023-03-15 17:44:59 +0800

    'update:giscus安全配置'

--- next.config.js
+++ next.config.js
@@ -4,14 +4,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  media-src 'none';
-  connect-src *;
-  font-src 'self';
-  frame-src giscus.app
+default-src 'self';
+script-src * blob: data: 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+style-src * blob: data: 'self' 'unsafe-inline';
+img-src * blob: data:;
+media-src * blob: data:;
+connect-src * blob: data:;
+font-src 'self';
+frame-src * blob: data: giscus.app;
+worker-src * blob: data:;
 `
 
 const securityHeaders = [
